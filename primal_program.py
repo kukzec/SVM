5a0cd77adced35904c6c1119ae51719b772fb624
lpalma <loic.palma@outlook.com>
2019-09-03 10:08:21 +0200

    First Exercice: Reproducing the primal program on python

---
+++ primal_program.py
@@ -0,0 +1,25 @@
+import numpy as np
+from cvxopt import matrix
+from cvxopt import solvers
+
+var_all_numpy= np.array([[4,2,4,6,8,6,12,10,8,6],[8,4,6,6,8,10,6,6,2,2]])
+var_1_numpy = np.array([4,2,4,6,8,6,12,10,8,6])
+var_2_numpy = np.array([8,4,6,6,8,10,6,6,2,2])
+y_numpy = np.array([-1,-1,-1,-1,-1,-1,1,1,1,1])
+
+H = matrix(np.array([[1,0,0],[0,1,0],[0,0,0]]),tc='d')
+C = -matrix(np.array([1,1,1,1,1,1,1,1,1,1]),tc='d')
+test_1 = np.multiply(var_1_numpy,y_numpy)
+test_2 = np.multiply(var_2_numpy,y_numpy)
+A = -matrix([[-4,-2,-4,-6,-8,-6,12,10,8,6],[-8,-4,-6,-6,-8,-10,6,6,2,2],[-1,-1,-1,-1,-1,-1,1,1,1,1]],tc='d')
+zeros=matrix(np.zeros((3,1)),tc='d')
+
+sol = solvers.qp(H,zeros,A,C)
+print(sol['primal objective'])
+print(sol['x'])
+
+
+
+
+
+
